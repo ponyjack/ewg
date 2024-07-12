3b61ee178fa4f422485cff0aba0ef8edc24dda7d
ponyjack <42662868+ponyjack@users.noreply.github.com>
2024-07-12 22:08:07 +0800

    add log

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
-apt update
-apt install -y python3-pip
+# apt update
+# apt install -y python3-pip
 python3 -m pip install loguru requests schedule
 
 nohup python3 main.py >output.log 2>&1 &
+tail -f output.log
